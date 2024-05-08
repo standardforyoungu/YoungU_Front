a00b7d6b092dcfaeb7ad5a079a8727bc905847b9
mia <tjwlfif@gmail.com>
2024-05-09 08:16:22 +0900

    [feat]: tailwind screen 설정 추가, innerWith 상태 관련 훅 추가

---
+++ src/hooks/useGetWindowSize.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+export const useGetWindowSize = () => {
+	const [screen, setScreen] = useState<"WEB" | "TABLET" | "MOBILE">()
+
+	const handleResize = () => {
+		if (window.innerWidth >= 1024) {
+			setScreen("WEB")
+		} else if (window.innerWidth >= 431 && window.innerWidth <= 1023) {
+			setScreen("TABLET")
+		} else if (window.innerWidth <= 430) {
+			setScreen("MOBILE")
+		}
+	}
+
+	useEffect(() => {
+		handleResize()
+		window.addEventListener("resize", handleResize)
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
+
+	return screen
+}
