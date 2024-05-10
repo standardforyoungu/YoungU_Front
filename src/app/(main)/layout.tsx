be58a7af4903534e1c531b287a396a04bd8f3b31
mia <tjwlfif@gmail.com>
2024-05-10 21:30:00 +0900

    [fix]: layout 수정

--- src/app/(main)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import Layout from "@/components/Layout";
-import React from "react";
-
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-	return <Layout>{children}</Layout>;
-}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import { useGetWindowSize } from "@/hooks/useGetWindowSize";
 import Header from "./@commons/Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
-	const { screen } = useGetWindowSize();
 	return (
 		<div className="min-w-screen w-full flex justify-center items-center gap-[80px] h-full min-h-screen bg-gray-95">
-			{screen === "WEB" && (
-				<div className="flex justify-center items-center w-[360px] h-[800px] bg-gray-40">광고 영역</div>
-			)}
+			<div className="sm:hidden md:hidden flex justify-center items-center w-[360px] h-[800px] bg-gray-40">
+				광고 영역
+			</div>
 			<div className="sm:min-w-[360px] sm:max-w-[430px] sm:w-full sm:mx-auto w-[500px] overflow-scroll max-h-screen relative scrollbar-hide ">
 				<Header />
 				{children}
