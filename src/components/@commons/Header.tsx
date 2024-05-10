7768a072d2c659adeaec8d3e77d41cca7ba23e52
mia <tjwlfif@gmail.com>
2024-05-10 20:48:03 +0900

    [fix]: header png -> svg 변경

--- src/components/@commons/Header.tsx
+++ src/components/@commons/Header.tsx
@@ -4,8 +4,8 @@ import React from "react";
 function Header() {
 	return (
 		<header className="flex justify-between px-[20px] py-[16px] bg-WHITE border-b border-[#0000001a] w-full sticky top-0 z-10 ">
-			<Image src={"/icons/main_symbol.png"} alt={"symbol"} width={30} height={32} />
-			<Image src={"/icons/user.png"} alt={"user"} width={32} height={32} />
+			<Image src={"/icons/main_symbol.svg"} alt={"symbol"} width={30} height={32} />
+			<Image src={"/icons/user.svg"} alt={"user"} width={32} height={32} />
 		</header>
 	);
 }
