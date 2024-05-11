730a45f74ebc6bf606fc10fc5dbbcacb192bc249
bangbongbim <dkdpdl12311@gmail.com>
2024-05-11 19:23:49 +0900

    [feat] : 게시글 리스트 아이템 컴포넌트 디자인 제작

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,7 +1,38 @@
+import ListItem from "@/components/ListItem/Index";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination";
+
 import React from "react";
 
 function page() {
-	return <div>page</div>;
+	return (
+		<div className="h-full">
+			<ListItem />
+			<Pagination>
+				<PaginationContent>
+					<PaginationItem>
+						<PaginationPrevious href="#" />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">1</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationNext href="#" />
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
+		</div>
+	);
 }
 
 export default page;
---
+++ src/components/ListItem/Index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+
+import phoneIcon from "../../../public/icons/phone-call.svg";
+import mapIcon from "../../../public/icons/map-pin.svg";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+function ListItem() {
+	const router = useRouter();
+
+	const onClickHandler = () => {
+		router.push("http://3.36.118.161/test");
+	};
+
+	return (
+		<div className="flex flex-col max-h-[165]   w-full py-[1.5rem] px-[1.25rem] bg-WHITE border-b border-[#0000001a]">
+			<h4 className="head4 text-gray-20 my-[0.2rem]">쥬빌리프라임주니어(JUBILEEPRIME)어학학원</h4>
+			<div className="flex flex-row my-[0.2rem]">
+				<Image src={phoneIcon} alt="phoneIcon" />
+				<p className="body2 text-gray-40">정보 010-9805-7346</p>
+			</div>
+			<div className="flex flex-row my-[0.2rem]">
+				<Image src={mapIcon} alt="mapIcon" />
+				<p className="body2 text-gray-40">정보 서울시 성내동 502-1</p>
+			</div>
+
+			<Button variant={"primary"} size={"lg"} className="my-[0.2rem]" onClick={onClickHandler}>
+				홈페이지 바로가기
+			</Button>
+		</div>
+	);
+}
+
+export default ListItem;
