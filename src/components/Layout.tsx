a1c64ce5c4c8078115ea515f74df938abefe2979
mia <tjwlfif@gmail.com>
2024-05-11 14:34:39 +0900

    [fix]: layout 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import Header from "./@commons/Header";
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<div className="min-w-screen w-full flex justify-center items-center gap-[80px] h-full min-h-screen bg-gray-95">
-			<div className="sm:hidden md:hidden flex justify-center items-center w-[360px] h-[800px] bg-gray-40">
+			<div className="sm:hidden md:hidden flex justify-center items-center w-[360px] h-screen bg-gray-40">
 				광고 영역
 			</div>
 			<div className="sm:min-w-[360px] sm:max-w-[430px] sm:w-full sm:mx-auto w-[500px] overflow-scroll max-h-screen relative scrollbar-hide ">
