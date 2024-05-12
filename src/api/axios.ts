cddf85ad65950e5150b77e279e3a73f8a6b6a53e
mia <tjwlfif@gmail.com>
2024-05-12 20:48:07 +0900

    [fix]: axios instance 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_DOMAIN,
+	baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN,
 	withCredentials: true,
 });
-
-// TODO_JIYEON: accessToken 없을 때 작업 추가 해야함
