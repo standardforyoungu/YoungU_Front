64aabf7dd0bd96d460efe3387ff7405c87c85bfe
mia <tjwlfif@gmail.com>
2024-05-21 16:24:03 +0900

    [fix]: 로그인 여부에 따른 페이징

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,19 +1,22 @@
-"use client"
+"use client";
 
-import { usePathname, useRouter } from "next/navigation"
-import React, { useEffect } from "react"
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export default function AuthProvider() {
-	const pathname = "/" + usePathname().split("/")[1]
-	const router = useRouter()
+	const pathname = usePathname();
+	const path = pathname.includes("my-page") ? pathname.split("/my-page")[1] : pathname;
+	const router = useRouter();
 
 	useEffect(() => {
-		/**
-		 * TODO_JIYEON:
-		 * 로그인 상태면 return
-		 * 로그인 하지 않으면 접근할 수 없는 페이지들 처리
-		 */
-	}, [])
+		if (!!window.localStorage.getItem("OU_UserAttribute")) {
+			path.includes("oauth") && router.push("/");
+		} else {
+			["/test", "/setting", "/child-list", "/child-list/add-child", "/child-list/edit-child"].includes(path) &&
+				router.back();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [path]);
 
-	return <></>
+	return <></>;
 }
