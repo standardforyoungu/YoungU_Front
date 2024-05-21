a5e15545c6353c4aff2a950748f87f05251e422f
mia <tjwlfif@gmail.com>
2024-05-21 16:22:52 +0900

    [feat]: 메인 페이지 디자인 수정

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,18 +5,18 @@ import MainCards from "../../components/home/MainCards";
 
 export default function Main() {
 	return (
-		<div className="w-full flex flex-col">
-			<Image className="absolute -left-4 top-32" src={"/icons/main_layer_1.svg"} alt={"layer"} width={72} height={72} />
-			<Image className="absolute left-20 top-28" src={"/icons/main_layer_2.svg"} alt={"layer"} width={15} height={15} />
+		<div className="w-full flex flex-col relative">
+			<Image className="absolute -left-4 top-24" src={"/icons/main_layer_1.svg"} alt={"layer"} width={72} height={72} />
+			<Image className="absolute left-20 top-20" src={"/icons/main_layer_2.svg"} alt={"layer"} width={15} height={15} />
 			<Image
-				className="absolute right-32 top-0"
+				className="absolute right-32 -top-16"
 				src={"/icons/main_layer_3.svg"}
 				alt={"layer"}
 				width={110}
 				height={110}
 			/>
-			<Image className="absolute right-0 top-20" src={"/icons/main_layer_4.svg"} alt={"layer"} width={92} height={98} />
-			<div className="bg-gray-99 w-full px-[20px] pb-[20px]">
+			<Image className="absolute right-0 top-10" src={"/icons/main_layer_4.svg"} alt={"layer"} width={92} height={98} />
+			<div className="bg-gray-99 w-full px-[20px] py-[20px]">
 				<div className="flex flex-col gap-[10px] justify-center items-center pt-[44px] pb-[28px] relative">
 					<Image
 						src={"/icons/beta_tag.svg"}
