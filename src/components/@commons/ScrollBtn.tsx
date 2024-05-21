8f8589f1867488faddcddfa093e916628ff5166c
mia <tjwlfif@gmail.com>
2024-05-21 22:47:01 +0900

    [fix]: 스크롤 버튼 변경

--- src/components/@commons/ScrollBtn.tsx
+++ src/components/@commons/ScrollBtn.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
 
 interface ScrollBtnProps {
@@ -16,9 +16,8 @@ export default function ScrollBtn({ showBtn, scrollTopHandler }: ScrollBtnProps)
 			onClick={onClickHandler}
 			className={`${
 				!showBtn && "hidden"
-			} w-14 h-14 bg-orange-100 rounded-full flex justify-center items-center fixed bottom-5 lg:left-[850px] md:left-[405px] sm:left-[320px] inset-x-0 z-50 mx-auto`}>
+			} w-14 h-14 bg-orange-100 rounded-full flex justify-center items-center fixed bottom-5 right-5`}>
 			<Image src={"/icons/arrow-right.svg"} alt="scrollToTop" width={20} height={20} className="-rotate-90" />
 		</button>
 	);
 }
-// `bg-WHITE fixed inset-x-0 bottom-[45%] z-50 mt-24 w-[320px] mx-auto lg:left-[440px] flex h-auto flex-col rounded-[8px] p-6 shadow-lg gap-[26.5px]`,
