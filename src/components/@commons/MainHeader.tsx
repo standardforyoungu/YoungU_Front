8855c6ff7bd7cb8ac7e48af78a90587af22a74ec
mia <tjwlfif@gmail.com>
2024-05-21 14:27:35 +0900

    [fix]: 메인 페이지 레이아웃 추가 & 헤더 수정

--- src/components/@commons/MainHeader.tsx
+++ src/components/@commons/MainHeader.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Header() {
+function MainHeader() {
 	return (
-		<header className="flex justify-between px-[20px] py-[16px] bg-WHITE border-b border-[#0000001a] w-full sticky top-0 z-10 ">
+		<header className="flex justify-between items-center px-[20px] py-[16px] bg-WHITE border-b border-[#0000001a] w-full sticky top-0 z-10 ">
 			<Link href={"/"}>
 				<Image src={"/icons/main_symbol.svg"} alt={"symbol"} width={30} height={32} />
 			</Link>
 			<Link href={"/my-page"}>
-				<Image src={"/icons/user.svg"} alt={"user"} width={32} height={32} />
+				<Image src={"/icons/icn_person.svg"} alt={"user"} width={26} height={26} />
 			</Link>
 		</header>
 	);
 }
 
-export default Header;
+export default MainHeader;
