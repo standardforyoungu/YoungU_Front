411748e8541d4189fdb66fc4f1663c6ca80ec32a
mia <tjwlfif@gmail.com>
2024-05-23 19:29:33 +0900

    [fix]: 로그인 onError 추가

--- src/app/(auth)/oauth/page.tsx
+++ src/app/(auth)/oauth/page.tsx
@@ -7,13 +7,13 @@ import React, { useEffect } from "react";
 export default function AuthPage() {
 	const searchParams = useSearchParams();
 	const authCode = searchParams.get("code");
-	const { mutate, onSuccess } = usePostLoginService();
+	const { mutate, onSuccess, onError } = usePostLoginService();
 
 	useEffect(() => {
 		if (!authCode) {
 			return;
 		} else {
-			mutate(authCode, { onSuccess });
+			mutate(authCode, { onSuccess, onError });
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
