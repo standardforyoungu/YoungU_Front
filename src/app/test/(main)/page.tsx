e517a17f005fb056132ba1fc67cb50f74739c34e
mia <tjwlfif@gmail.com>
2024-06-01 06:35:09 +0900

    [fix]: test page 스타일 수정

--- src/app/test/(main)/page.tsx
+++ src/app/test/(main)/page.tsx
@@ -14,22 +14,22 @@ export default function TestPage() {
 				<div className="w-full flex justify-center">
 					<InfoBadge title="검사 소개" />
 				</div>
-				<div className="w-full h-auto pt-10">
-					<p className="head5 text-center">
+				<div className="w-full h-auto pt-6">
+					<p className="body2 text-center text-gray-10">
 						더 이상 선택이 아닌 영어 유치부, <br />
 						우리 아이와 잘 맞는 영어유치부는 어디일까요?
 					</p>
-					<p className="head3 text-center pt-5">
+					<p className="head3 text-center pt-[6px] text-gray-10">
 						우리 아이 성향검사를 기반으로 <br />
 						<strong className="text-orange-100">맞춤 영유</strong>를 <strong className="text-orange-100">추천</strong>
 						해줄게요
 					</p>
 				</div>
-				<div className="flex mt-10 justify-center gap-2">
+				<div className="flex mt-6 justify-center gap-2">
 					<IconBadge title="총 문항수" highlight="20문제" iconHref="/icons/list.svg" />
 					<IconBadge title="예상 소요시간" highlight="약 5분" iconHref="/icons/timer.svg" />
 				</div>
-				<div className="flex flex-col items-center justify-center mt-20">
+				<div className="flex flex-col items-center justify-center mt-10">
 					<div className="w-[10rem] h-[10rem] relative">
 						<Image src="/images/infoPic.svg" alt="infoPic" layout="fill" objectFit="cover" className="w-full" />
 					</div>
@@ -40,7 +40,7 @@ export default function TestPage() {
 					</Link>
 				</div>
 			</ContentBox>
-			<div className="w-full h-auto px-5 py-12 bg-[#EDEDED] text-gray-60">
+			<div className="w-full h-auto px-5 py-12 bg-gray-97 text-gray-60">
 				<p className="head4 my-3">검사 전 확인해주세요.</p>
 				<ul className="list-disc px-5">
 					{footerText.map((data, _idx) => (
--- src/components/@layouts/ContentBox.tsx
+++ src/components/@layouts/ContentBox.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function ContentBox({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='w-full px-[2rem] py-[1rem] my-10'>{children}</div>
-  )
+	return <div className="w-full px-[2rem] mt-10 my-12">{children}</div>;
 }
