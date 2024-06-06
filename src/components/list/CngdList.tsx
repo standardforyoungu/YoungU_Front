fc4476d26e5291de4ce11a2ba59d0af3b904f3f0
mia <tjwlfif@gmail.com>
2024-06-06 15:10:17 +0900

    [fix]: CngdList에서 정보 문구 삭제

--- src/components/list/CngdList.tsx
+++ src/components/list/CngdList.tsx
@@ -18,11 +18,11 @@ export default function CngdList({ title, phone, address, link }: ListItemInterf
 			<h4 className="head4 text-gray-20 my-1">{title}</h4>
 			<div className="flex gap-[2.5px] items-center my-[0.2rem]">
 				<Image src={"/icons/phone-call.svg"} alt="phon" width={16} height={16} />
-				<p className="body2 text-gray-40">정보 {phone}</p>
+				<p className="body2 text-gray-40">{phone}</p>
 			</div>
 			<div className="flex gap-[2.5px] items-center my-[0.2rem]">
 				<Image src={"/icons/map-pin.svg"} alt="pin" width={16} height={16} />
-				<p className="body2 text-gray-40">정보 {address}</p>
+				<p className="body2 text-gray-40">{address}</p>
 			</div>
 			<Link
 				className={cn(buttonVariants({ variant: link ? "list-big" : "disable", size: "lg" }), "mt-3 rounded-[8px]")}
