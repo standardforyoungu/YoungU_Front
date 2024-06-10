86b539693bfea7213b2b0d8dc17a679781cb0fea
mia <139830617+rmsoft-zena@users.noreply.github.com>
2024-06-10 22:43:23 +0900

    [fix]: 성향테스트 결과 헤더 수정

---
+++ src/app/test/result/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<header className="flex items-center justify-between px-[20px] py-[16px] head3 bg-White border-b border-[#0000001a] w-full sticky top-0 z-10 ">
+				<p className="text-gray-10">성향검사 결과</p>
+				<Link href={"/"}>
+					<Image src={"/icons/home.svg"} alt="home" width={24} height={24} />
+				</Link>
+			</header>
+			{children}
+		</>
+	);
+}
