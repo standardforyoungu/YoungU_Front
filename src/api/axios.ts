b6997cd2daa22addf25065f75e09733e654ef7fe
mia <139830617+rmsoft-zena@users.noreply.github.com>
2024-06-14 23:55:53 +0900

    [feat]: axios 405에러 시 로그아웃 처리

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,4 @@
+import { toast } from "@/utils/toast";
 import axios from "axios";
 
 export const http = axios.create({
@@ -19,6 +20,24 @@ http.interceptors.request.use(
 		return config;
 	},
 	function (error) {
+		return Promise.reject(error);
+	},
+);
+
+http.interceptors.response.use(
+	function (response) {
+		return response;
+	},
+	function (error) {
+		// 로그아웃
+		if (error.response?.status === 405) {
+			if (typeof window !== "undefined") {
+				window.localStorage.removeItem("OU_UserAttribute");
+				window.localStorage.removeItem("mbr_id");
+				location.replace("/");
+				toast("Error", "토큰이 만료되었습니다. 다시 로그인 해주세요.");
+			}
+		}
 		return Promise.reject(error);
 	},
 );
