1535452e736048ec847be0cd10951177d683a537
mia <139830617+rmsoft-zena@users.noreply.github.com>
2024-06-19 21:45:10 +0900

    [fix]: scroll btn shadow 추가

--- src/components/@commons/ScrollBtn.tsx
+++ src/components/@commons/ScrollBtn.tsx
@@ -13,7 +13,7 @@ export default function ScrollBtn({ scrollTopHandler }: ScrollBtnProps) {
 	return (
 		<button
 			onClick={onClickHandler}
-			className={`w-14 h-14 bg-orange-100 rounded-full flex justify-center items-center fixed bottom-5 right-5`}>
+			className={`w-14 h-14 bg-orange-100 rounded-full flex justify-center items-center fixed bottom-5 right-5 !shadow-2xl`}>
 			<Image src={"/icons/arrow.svg"} alt="scrollToTop" width={20} height={20} className="-rotate-90" />
 		</button>
 	);
