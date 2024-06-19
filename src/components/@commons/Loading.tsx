c995a733b82a5df7ba7547a4b3a9c96fb885e0a4
mia <139830617+rmsoft-zena@users.noreply.github.com>
2024-06-19 21:48:09 +0900

    [fix]: loading 페이지 배경색 변경

--- src/components/@commons/Loading.tsx
+++ src/components/@commons/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading() {
 	return (
-		<div className="bg-orange-10 w-full h-screen bg-[url('/images/pettern.svg')] flex flex-col gap-[18px] justify-center items-center">
+		<div className="bg-orange-10/10 w-full h-screen bg-[url('/images/pettern.svg')] flex flex-col gap-[18px] justify-center items-center">
 			<div className="relative">
 				<div className="w-12 h-12 bg-gradient-to-br from-orange-200 to-orange-10 rounded-full animate-spin" />
 				<div className="w-8 h-8 bg-orange-10 rounded-full absolute top-2 right-[7.5px]" />
