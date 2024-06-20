81aa0ed999be72af1d0cb08ed2912bb93a480ece
mia <tjwlfif@gmail.com>
2024-06-20 14:54:18 +0900

    [feat]: metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,20 @@ import { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
 	title: "영유스탠다드",
+	description: "영어 유치부가 고민될땐? 성향검사를 통해 아이에게 맞는 학습법을 찾아보세요!",
+	keywords: [],
+	openGraph: {
+		title: "영유스탠다드",
+		description: "영어 유치부가 고민될땐? 성향검사를 통해 아이에게 맞는 학습법을 찾아보세요!",
+		url: "https://young-u-standard.site/",
+		siteName: "영유스탠다드",
+		images: {
+			url: "https://youngu-s3-bucket.s3.ap-northeast-2.amazonaws.com/propensity_result/OG_800x400.jpg",
+			width: 800,
+			height: 400,
+		},
+		type: "website",
+	},
 };
 
 export const viewport: Viewport = {
