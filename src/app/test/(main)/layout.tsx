4c0531350d30778fd858e24863e6fae298abec1a
mia <tjwlfif@gmail.com>
2024-06-26 18:09:32 +0900

    [fix]: 성향검사 안내 페이지 layout metadata 수정

--- src/app/test/(main)/layout.tsx
+++ src/app/test/(main)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ListHeader from "@/components/@commons/ListHeader";
 
 export const metadata: Metadata = {
-	title: "성향검사 | 영유스탠다드",
+	title: "성향검사 안내 | 영유스탠다드",
 };
 
 export default function layout({ children }: { children: React.ReactNode }) {
